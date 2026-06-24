afdd7ce96f2cc98b3be9ce3bdb259f287976e53f
olseinmelea-jpg <olseinmelea@gmail.com>
2026-06-24 22:06:39 +0100

    Create messageStyle.js

---
+++ utils/messageStyle.js
@@ -0,0 +1,19 @@
+import fs from "fs"
+import stylizedChar from "./fancy.js"
+
+export default function stylizedCardMessage(text) {
+  return {
+    text: stylizedChar(text),
+    contextInfo: {
+      externalAdReply: {
+        title:
+"ROGUE_BOLT-DRAGON",
+        body: "𓆩 BOLT DEV𓆪",
+        thumbnail: fs.readFileSync("./database/DigiX.jpg"),
+        sourceUrl: "https://whatsapp.com",
+        mediaType: 1,
+        renderLargerThumbnail: false
+      }
+    }
+  }
+}
